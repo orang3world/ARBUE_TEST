2f3aa96c9cf5559c85a9f6834d5a92e1e63a6605
orang3world <aaorange75@gmail.com>
2023-05-20 22:00:44 -0300

    El codigo html para los informes academicos (emails) fue pasado a una planilla: "html"

--- csvProcess.js
+++ csvProcess.js
@@ -83,6 +83,7 @@ var csvIds = []     // Array for csv File ids
 var instructorName = {}
 var instructorLinkedin = {}
 var instructorCel = {}
+var instructorRole = {}
 
 // Regex
 //example of csv name = /participant-20230502222213.csv/
@@ -244,60 +245,69 @@ function informeAcademico() {
   SpreadsheetApp.getActiveSpreadsheet().toast(
     'Inicio del envio de e-mails personalizados '
     , 'ENVIANDO Informe Academico ...');
+
+  var email_prof = Session.getActiveUser().getEmail();
+  var arrayInstructorData = dataRangeInstructorData.slice(1)
+
+  for (let i = 0; i < arrayInstructorData.length; i++) {
+    var iEmail = arrayInstructorData[i][0]
+    var iName = arrayInstructorData[i][1]
+    var iLink = arrayInstructorData[i][2]
+    var iCel = arrayInstructorData[i][3]
+    var iRole = arrayInstructorData[i][4]
+
+    Object.assign(instructorName, { [iEmail]: iName })
+    Object.assign(instructorLinkedin, { [iEmail]: iLink })
+    Object.assign(instructorCel, { [iEmail]: iCel })
+    Object.assign(instructorRole, { [iEmail]: iRole })
+  }
+
+  var body = dataRangeBody[0][1]
+
+  var signature = dataRangeSignature[0][1]
+  signature = signature.replace("{{instructorName[email_prof]}}", instructorName[email_prof])
+  signature = signature.replace("{{instructorLinkedin[email_prof]}}", instructorLinkedin[email_prof])
+  signature = signature.replace("{{instructorCel[email_prof]}}", instructorCel[email_prof])
+  signature = signature.replace("{{instructorRole[email_prof]}}", instructorRole[email_prof])
+
   /* array datas in update-kc ----*/
   var dataRangeUPDATEKC = ssUpdateKc.getRange(2, 1, ssUpdateKc.getLastRow() - 1, ssUpdateKc.getLastColumn()).getValues()
+  var variableName = ["{{email}}", "{{student}}", "{{attendance}}", "{{kcok}}", "{{state2}}", "{{totalfalt}}", "{{notDone}}", "{{notDoneKcList}}", "{{lowGrade}}", "{{lowGradeKcList}}"]
 
   /* iterate through array rows --*/
   dataRangeUPDATEKC.forEach(
 
     function crearMensaje(value) {
-      /* VARIABLES DENTRO DEL E-MAIL */
-
-      var email = value[0];
-      var ccemail = "aaorange75@gmail.com"// "potrerodigital@compromiso.org";
-      var student = value[1];
-      var attendance = value[2];
-      var kcok = value[3];
-      var state2 = value[4];
-      var totalfalt = value[5];
-      var notDone = value[6];
-      var notDoneKcList = value[7];
-      var lowGrade = value[8];
-      var lowGradeKcList = value[9];
-
-      var arrayInstructorData = dataRangeInstructorData.slice(1)
-      var email_prof = Session.getActiveUser().getEmail();
-
-      for (let i = 0; i < arrayInstructorData.length; i++) {
-        var iEmail = arrayInstructorData[i][0]
-        var iName = arrayInstructorData[i][1]
-        var iLink = arrayInstructorData[i][2]
-        var iCel = arrayInstructorData[i][3]
-
-        Object.assign(instructorName, { [iEmail]: iName })
-        Object.assign(instructorLinkedin, { [iEmail]: iLink })
-        Object.assign(instructorCel, { [iEmail]: iCel })
-        
+      var variable = {}
+      /*VARIABLES DENTRO DEL E-MAIL */
+      for (let i = 0; i < variableName.length; i++) {
+        if (i == 1) {
+          Object.assign(variable, { "{{student}}": value[1].toString().toUpperCase() })
+          body = body.replace([variableName[i]], value[1].toString().toUpperCase())
+        } else {
+          Object.assign(variable, { [variableName[i]]: value[i] })
+          body = body.replace([variableName[i]], value[i])
+        }
       }
-
-      //var signature = dataRangeSignature[0][0]
-      //var body_html = HtmlService.createHtmlOutput(dataRangeBody[0][0])
+      body = body.replace("{{signature}}", signature)
 
       /* utilizar https://codepen.io/ para el armado del HTML */
+      /* usar https://www.textfixer.com/html/compress-html-compression.php para generar html en una sola linea */
 
-      var signature = '<table cellspacing="0" cellpadding="0" border="0" style="width: 100%;"> <tbody> <tr class="signature" style=""> <td valign="top" align="center" class="center-on-narrow stack-column" style="width: 100%; overflow: hidden;"> <div style="border: 0px solid transparent;color: #f1f4f6; display: block; font-family: Helvetica, Arial, sans-serif; font-size: 10px; font-weight: normal; line-height: 1.2; Margin: 25px 20px 25px 25px; max-height: none; max-width: none; padding: 0px; text-decoration: none;"> <div> <font color="#f1f4f6" face="Arial, Helvetica, sans-serif" size="4" class="nombre"> <b>' + instructorName[email_prof] + '</b> | Instructor </font> </div> <div> <font color="#f1f4f6" face="Arial, Helvetica, sans-serif" size="3">AWS Certified Associate<br></font> <div class=""> <font size="3"><b> <font face="&quot;Arial&quot;, Helvetica, sans-serif"> <a href="http://compromiso.org/" style="color: #ec7211;" class="">compromiso.org</a> </font> </b></font> <font color="#f1f4f6" face="Arial, Helvetica, sans-serif" size="3">| </font> <font size="3"><b> <font face="&quot;Arial&quot;, Helvetica, sans-serif"> <a href="http://potrerodigital.org/" class="" style="color: #ec7211;">potrerodigital.org</a> </font> </b></font> <br> </div> <div class="firm"> <font color="#f1f4f6" face="Arial, Helvetica, sans-serif" size="3"> Cel. +'+ instructorCel[email_prof] +' </font> <font color="#f1f4f6" face="Arial, Helvetica, sans-serif" size="3">| </font> <font size="2"><b> <font face="&quot;Arial&quot;, Helvetica, sans-serif"> <a href="' + instructorLinkedin[email_prof] + '" class="" style="color: #ec7211;">LinkedIn</a> </font> </b></font> <br> </div> </div> </div> </td> </tr> <tr class="signature"> <td valign="top" align="center" class="center-on-narrow stack-column" style="width: 100%; overflow: hidden; vertical-align: bottom; padding-bottom: 15px;"> <img src="https://ci4.googleusercontent.com/proxy/ZDZm9x8NP-OYdpemo9sXn8iq8qc7K4FslWZBBTo1zg21pCjv13Ph6KMzbIU0g2oAkEU71HA-gMsWoqGwPYj7vWpnD7xZARpNotH0BnphC7aMccAc7618dQO8o_dLzeDBDGu2yiynJvrX1or5KzfM=s0-d-e1-ft#https://images.credly.com/size/340x340/images/00634f82-b07f-4bbd-a6bb-53de397fc3a6/image.png" alt="AWS Certified Cloud Practitioner" width="96" height="96" class="CToWUd" data-bit="iit"> <img src="https://ci3.googleusercontent.com/proxy/VpBzkJjDHaXcQzbidXDpRdPSPahgJpbU0hDNcIlrLBYVjLsxUevGLzYdvNk6KUL8LCGpejdGVl6U02zuSjo3ga91sTAokXYo1Tm1Z3t0Kc1p6h4xg6shcgYuMVmy_rP_SmFkXtreax1p1qLovE3N=s0-d-e1-ft#https://images.credly.com/size/340x340/images/0e284c3f-5164-4b21-8660-0d84737941bc/image.png" alt="AWS Certified Solutions Architect – Associate" width="96" height="96" class="CToWUd" data-bit="iit"> <img src="https://ci3.googleusercontent.com/proxy/ep9w9gvBrTwl4kJ19bQf0B3BaFV-O9Bfd1ooVx2pJWVong0E4Sxa2NdRdZ5Atfs36cC13_SfW3IeTqCmO9lneyty4VFwiSvX7QC096MWg_sDU0o8-EvZBX1FWgbS5FH3q1DWlDqudPGTeUEpNpiM=s0-d-e1-ft#https://images.credly.com/size/340x340/images/44e2c252-5d19-4574-9646-005f7225bf53/image.png" alt="AWS re/Start Graduate" width="96" height="96" class="CToWUd" data-bit="iit"> <img src="https://ci6.googleusercontent.com/proxy/R-aIAR8Uu3ffYUJisbl6bfL1YWUP8r_JXip-EIbcQV-er5fz-wbsvK4HA7qItSyDrVV70pze-9hgT6MPwt2pfkYAIUtPwNtIASl_1MxA1MCaVn4soT1EGZpv5XsakDHw8PqTsVXIvbFtVLfQaZqw=s0-d-e1-ft#https://images.credly.com/size/340x340/images/e426d40e-8a6a-4f72-866e-2abfcfbde46b/image.png" alt="AWS re/Start Accredited Instructor" width="96" height="96" class="CToWUd" data-bit="iit"> </td> </tr> </tbody> </table>';
-      var body_html = HtmlService.createHtmlOutput('<html><head> <meta charset="utf-8"> <meta name="viewport" content="width=device-width"> <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="robots" content="noindex"> <base target="_blank"> <style type="text/css"> body, div[style*="margin: 16px 0"], html { margin: 0 !important } body, html { padding: 0 !important; height: 100% !important; width: 100% !important } * { -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100% } table, td { mso-table-lspace: 0 !important; mso-table-rspace: 0 !important } table { border-spacing: 0 !important; border-collapse: collapse !important; margin: 0 auto !important } table table table { table-layout: auto } img { -ms-interpolation-mode: bicubic } .yshortcuts to { border-bottom: none !important } .mobile-link--footer a, a[x-apple-data-detectors] { color: inherit !important; text-decoration: underline !important } .signature{ background-color: #232f3e; } @media screen and (max-width:600px) { .stack-column-half { width: 50% !important; display: inline-block !important } .center-on-narrow, .fluid, .fluid-centered { margin-left: auto !important; margin-right: auto !important } table { table-layout: fixed !important } .email-container { width: 100% !important } .fluid, .fluid-centered { max-width: 100% !important; height: auto !important } .stack-column, .stack-column-center, .stack-column-full-width { display: block !important; width: 100% !important; max-width: 100% !important; direction: ltr !important } .center-on-narrow { display: block !important; float: none !important } table.center-on-narrow { display: inline-block !important } .stack-column-full-width .eddie-wrapper { color: white; width: 100% } } </style></head><body leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0"> <table cellspacing="0" cellpadding="0" border="0" width="100%" style="font-family: Helvetica, Arial, sans-serif; width: 100%; padding: 20px; background-color: rgb(235, 235, 235); background-image: none;"> <tbody> <tr> <td align="center"> <table class="email-container" width="660"> <tbody> <tr> <td> <div class="eddie-page"> <!-- barra inicial --> <table cellspacing="0" cellpadding="0" border="0" style="width: 100%;"> <tbody> <tr style="background-color: #232f3e;"> <td valign="top" align="center" stackclass="stack-column-full-width" class="stack-column-full-width" style="width: 50%; overflow: hidden;"> <img src="https://d1.awsstatic.com/training-and-certification/Logos/aws_restart_logo_reverse.860113148166c4742ebd63e8fa74d09ae4cf64ea.png" width="auto" height="auto" class="fluid" style="border: 0px solid transparent;width:50%; color: rgb(0, 0, 0); display: block; font-family: Helvetica, Arial, sans-serif; font-size: 10px; font-weight: normal; line-height: 1.35; Margin: 5px; max-height: none; max-width: none; padding: 5px; text-decoration: none; min-height: 10px;" alt=""> </td> <td valign="top" align="left" stackclass="stack-column-full-width" class="stack-column-full-width" style="width: 50%; overflow: hidden;"> <img src="https://static.wixstatic.com/media/5b90eb_2f1f983af79a4e69ba942bc0586dbb7d~mv2.png/v1/fill/w_382,h_40,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/potrero_digital_2021_edited.png" width="auto" height="auto" class="fluid" style="border: 0px solid transparent;width: 80%; color: rgb(0, 0, 0); display: block; font-family: Helvetica, Arial, sans-serif; font-size: 10px; font-weight: normal; line-height: 1.35; Margin: 10px; max-height: none; max-width: none; padding: 10px; text-decoration: none; min-height: 10px;" alt=""> </td> </tr> </tbody> </table> <!-- imagen grande --> <table cellspacing="0" cellpadding="0" border="0" style="width: 100%;"> <tbody> <tr style="background-color: #ec7211;"> <td valign="top" class="center-on-narrow stack-column" style="width: 100%; overflow: hidden;"> <div style="border: 0px solid transparent;color: #ec7211; display: block; font-family: Helvetica, Arial, sans-serif; font-size: 10px; font-weight: normal; line-height: 1.2; Margin: 25px 20px 25px 25px; max-height: none; max-width: none; padding: 0px; text-decoration: none;"> <div style="text-align:center;"> <font color="white" face="Arial, Helvetica, sans-serif" size="7"> <b>Informe Académico</b> </font> </div> </div> </td> </tr> </tbody> </table> <!-- cuerpo --> <table cellspacing="0" cellpadding="0" border="0" style="width: 700px; height: 528px;"> <tbody> <tr style="background-color: #ced2d5;"> <td valign="top" class="center-on-narrow stack-column" style="width: 100%; overflow: hidden;"> <div style="border: 0px solid transparent;color: #ec7211; display: block; font-family: Helvetica, Arial, sans-serif; font-size: 10px; font-weight: normal; line-height: 1.2; Margin: 25px 20px 25px 25px; max-height: none; max-width: none; padding: 0px; text-decoration: none;"> <div style=""> <font color="#000000" face="Arial, Helvetica, sans-serif" size="4"> <p><b>Informe académico de</b>: ' + student.toString().toUpperCase() + '</p> <p><b>Porcentaje de asistencia</b>: ' + attendance + ' %</p> <p><b>Porcentaje de KC realizados</b>: ' + kcok + ' %</p> <p><b>estado de los KC</b>: ' + state2 + '</p> <p><b>Cantidad de KC pendientes (sin hacer mas los que tienen baja nota)</b>: ' + totalfalt + '</p> <p><b>Cantidad de KC sin realizar</b>: ' + notDone + '</p> <p><b>Lista de los KC sin realizar</b>: </p> <ul>' + notDoneKcList + '</ul> <p><b>Cantidad de KC con baja nota</b>: ' + lowGrade + '</p> <p><b>Lista de KC con baja nota</b>:</p> <ul>' + lowGradeKcList + '</ul> <p>Cualquier inquietud, el grupo de docentes estamos para ayudarles.</p> <p>* Los KC con BAJA NOTA son aquiellos con menos del 70% de la nota maxima.</p> <p>* Recuerden realizar las "Notas of salida" (Exit Tickets).</p> </font> </div> </div> </td> </tr> </tbody> </table> <!--signature--> ' + signature + ' <!-- separador --> <table cellspacing="0" cellpadding="0" border="0" style="width: 100%;"> <tbody> <tr style="background-color: #ced2d5"> <td valign="top" class="stack-column" style="width: 100%; overflow: hidden;"> <div style="border: 0px solid transparent;color: rgb(0, 0, 0); display: block; font-family: Helvetica, Arial, sans-serif; font-size: 10px; font-weight: normal; line-height: 1.35; Margin: 1px 25px; max-height: none; max-width: none; padding: 0px; text-decoration: none; height: 3px; background-color: #232f3e;"> </div> </td> </tr> </tbody> </table> <!-- footer --> <table cellspacing="0" cellpadding="0" border="0" style="width: 100%;"> <tbody> <tr style="background-color: #232f3e;"> <td valign="top" align="center" class="stack-column" style="width: 100%; overflow: hidden;"> <span class="eddie-wrapper" style="display: inline-block; Margin: 20px 10px 20px 0px"><a href="https://www.facebook.com/potrerodigital/" data-interests="" style="text-decoration: none;"><img src="http://cdn.pemres02.net/10433/recurso-9.png" alt="Facebook" width="auto" height="auto" class="fluid" style="border: 0px solid transparent;width: auto; height: auto; display: inline-block; font-family: Helvetica, Arial, sans-serif; font-size: 10px; font-weight: normal; line-height: 1.35; Margin: 0px; max-height: none; max-width: none; padding: 0px; text-decoration: none;" data-margin="20px 10px 20px 0px"></a></span> <span class="eddie-wrapper" style="display: inline-block; Margin: 20px 10px 20px 0px"><a href="https://www.linkedin.com/company/potrero-digital/mycompany/" data-interests="" style="text-decoration: none;"><img src="http://cdn.pemres02.net/10433/recurso-8.png" alt="LinkedIn" width="auto" height="auto" class="fluid" style="border: 0px solid transparent;width: auto; height: auto; display: inline-block; font-family: Helvetica, Arial, sans-serif; font-size: 10px; font-weight: normal; line-height: 1.35; Margin: 0px; max-height: none; max-width: none; padding: 0px; text-decoration: none;" data-margin="20px 10px 20px 0px"></a></span> <span class="eddie-wrapper" style="display: inline-block; Margin: 20px 10px 20px 0px"><a href="https://www.instagram.com/potrerodigital/" data-interests="" style="text-decoration: none;"><img src="http://cdn.pemres02.net/10433/recurso-10.png" alt="Instagram" width="auto" height="auto" class="fluid" style="border: 0px solid transparent;width: auto; height: auto; display: inline-block; font-family: Helvetica, Arial, sans-serif; font-size: 10px; font-weight: normal; line-height: 1.35; Margin: 0px; max-height: none; max-width: none; padding: 0px; text-decoration: none;" data-margin="20px 10px 20px 0px"></a></span> <span class="eddie-wrapper" style="display: inline-block; Margin: 20px 10px 20px 0px"><a href="https://www.youtube.com/channel/UCkh0OTzDBAtqKtXjHFqQinQ" data-interests="" style="text-decoration: none;"><img src="http://cdn.pemres02.net/10433/recurso-11.png" alt="Youtube" width="auto" height="auto" class="fluid" style="border: 0px solid transparent;width: auto; height: auto; display: inline-block; font-family: Helvetica, Arial, sans-serif; font-size: 10px; font-weight: normal; line-height: 1.35; Margin: 0px; max-height: none; max-width: none; padding: 0px; text-decoration: none;" data-margin="20px 10px 20px 0px"></a></span> </td> </tr> </tbody> </table> </div> </td> </tr> </tbody> </table> </td> </tr> </tbody> </table></body></html>');
-
-      /* usar https://www.textfixer.com/html/compress-html-compression.php para 
-generar html en una sola linea */
-
-
-      var new_subject = "Informe Académico " + student.toString().toUpperCase() + " - AWS re/Start " + courseName;
+      var new_subject = "Informe Académico " + variable["{{student}}"] + " - AWS re/Start " + courseName;
       var empty_msj = "";
+      var ccemail = "aaorange75@gmail.com"
 
-      /* send email to each student -*/
-      GmailApp.sendEmail(email, new_subject, empty_msj, { cc: ccemail, attachments: body_html });
-    })
+      MailApp.sendEmail({
+        to: variable["{{email}}"],
+        cc: ccemail,
+        subject: new_subject,
+        body: empty_msj,
+        htmlBody: body
+      });
+    }
+  )
+
   return (SpreadsheetApp.getActiveSpreadsheet().toast(
     'todos los informes academicos han sido enviados '
     , 'FINALIZADO el envio de e-mails'))
